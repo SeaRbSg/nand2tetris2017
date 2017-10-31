164b042c1345add0d6c774df869beb0ab6359c73
John Fearnside <john@apptentive.com>
2017-10-31 12:44:51 -0700

    Implement Mult

--- jwfearn/04/mult/mult.asm
+++ jwfearn/04/mult/mult.asm
@@ -1,9 +1,29 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  @0       // i = R0
+  D=M
+  @i
+  M=D
+
+  @2       // R2 = 0
+  M=0
+
+(LOOP)
+  @i       // goto END if i == 0
+  D=M
+  @END
+  D;JEQ
+
+  @1       // R2 += R1
+  D=M
+  @2
+  M=M+D
+
+  @i       // i--
+  M=M-1
+
+  @LOOP    // goto LOOP
+  0;JMP
+
+(END)

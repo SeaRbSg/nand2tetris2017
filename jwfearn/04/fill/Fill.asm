278f8eed20c2e3e1d2e2a812774138d84bf14261
John Fearnside <john@apptentive.com>
2017-11-05 08:55:44 -0800

    Add Fill.asm pseudocode

--- jwfearn/04/fill/Fill.asm
+++ jwfearn/04/fill/Fill.asm
@@ -1,14 +1,22 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// @color=black (1)
+// goto WHITE
+(SCAN)
+  // if key goto BLACK
+(WHITE)
+  // if @color=white goto SCAN
+  // @color=white (0)
+(FILL)
+  // fill with @color
+  // goto SCAN
+(BLACK)
+  // if @color=black goto SCAN
+  // @color=black (1)
+  // goto FILL

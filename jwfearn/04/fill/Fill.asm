8cb71112152e54b7018b04f596a78ebd9a6632d7
John Fearnside <john@apptentive.com>
2017-11-05 10:44:46 -0800

    Add pointer calculation; use fill pattern

--- jwfearn/04/fill/Fill.asm
+++ jwfearn/04/fill/Fill.asm
@@ -6,38 +6,59 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-  @color   // @color = black (1)
-  M=1
+  @pattern // @pattern = black (-1 == 0b1111111111111111 == 16 black pixels)
+  M=-1
 
   @WHITE   // goto WHITE
   0;JMP
 
 (SCAN)
-  // if key goto BLACK
+  @KBD     // if KBD != 0 goto BLACK
+  D=M
+  @BLACK
+  D;JNE
 
 (WHITE)
-  @color   // if @color == white (0) goto SCAN
+  @pattern // if @pattern == white (0) goto SCAN
   D=M
   @SCAN
   D;JEQ
 
-  @color   // @color = white (0)
+  @pattern // @pattern = white (0)
   M=0
 
 (FILL)
-  // fill with @color
+  @8192    // @i = word length of screen (256 * 512 / 16 == 8192 == 8K)
+  D=A
+  @i
+  M=D
 
-  @SCAN    // goto SCAN
+(LOOP)
+  @i       // SCREEN[i] = @pattern
+  D=M
+  @SCREEN
+  A=A+D
+  //M=@pattern
+
+  @i       // goto SCAN if i == 0
+  D=M
+  @SCAN
+  D;JEQ
+
+  @i       // i--
+  M=M-1
+
+  @LOOP    // goto LOOP
   0;JMP
 
 (BLACK)
-  @color   // if @color != white (0) goto SCAN
+  @pattern // if @pattern != white (0) goto SCAN
   D=M
   @SCAN
   D;JNE
 
-  @color   // @color = black (1)
-  M=1
+  @pattern // @pattern = black (-1)
+  M=-1
 
   @FILL    // goto FILL
   0;JMP

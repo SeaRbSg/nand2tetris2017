487c0805790945fa8c40eb0fdd88585b2e2fd6f0
Scott Windsor <swindsor@gmail.com>
2017-11-10 20:06:19 -0800

    Got draw fill working. Need to hookup keyboard reading

--- sentientmonkey/04/fill/Fill.asm
+++ sentientmonkey/04/fill/Fill.asm
@@ -6,9 +6,32 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
+(BEGIN)
+    @SCREEN
+    D=A         // D=SCREEN
+    @position
+    M=D         // position=D
+    @24576      // SCREEN + 32 * 256 number of 16 bit pixel lines
+    D=A         // D=max
+    @max
+    M=D         // i=maximum
+    @draw       // draw=0
+    M=-1
+(NEXT)
+    @draw
+    D=M
+    @position
+    A=M         // move to position
+    M=D         // set position to black
+    @position
+    MD=M+1      // Advance position
+    @max
+    D=M-D       // D=max-D
+    @BEGIN
+    D;JEQ
+    @NEXT
+    0;JMP
